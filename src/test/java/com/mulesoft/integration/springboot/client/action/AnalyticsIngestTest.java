26d5ff0601d2c34a9d571c427e773d2932c02159
kapilduraphe <kapil.vduraphe@gmail.com>
2017-10-30 10:29:59 -0700

    adding some tests

--- src/test/java/com/mulesoft/integration/springboot/client/action/AnalyticsIngestTest.java
+++ src/test/java/com/mulesoft/integration/springboot/client/action/AnalyticsIngestTest.java
@@ -3,7 +3,7 @@
  * MuleSoft's Master Subscription Agreement (or other Terms of Service) separately entered into between you and MuleSoft. If such an
  * agreement is not in place, you may not use the software.
  */
-package com.mulesoft.integration.springboot.client.harness;
+package com.mulesoft.integration.springboot.client.action;
 
 
 import com.mulesoft.runtime.services.analytics.integration.model.MetricsIngestItem;
@@ -11,10 +11,12 @@ import com.mulesoft.runtime.services.analytics.integration.model.MetricsIngestIt
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import org.springframework.http.*;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.core.Is.is;
@@ -26,9 +28,13 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration
-public class QueryTest {
+@TestPropertySource("classpath:application-test.properties")
+public class AnalyticsIngestTest {
 
     private TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @Value("${analytics.query.url}")
+    private String analyticsQueueUrl;
 
     HttpHeaders headers = new HttpHeaders();
 
@@ -37,7 +43,7 @@ public class QueryTest {
         MetricsIngestItem ingestItem = new MetricsIngestItem("c3a49b88-6143-4f71-a586-d54ad345da3e", "1cc83dc2-3471-4492-bd1f-0d739b7b24f3", "us-east-1",
                 "test_store", "STORE", "API_EVENT", "2017-10-25T17:52:00.000Z", "0b7c83f9-0ffb-42f9-bbb8-813598e01e0a", 1L, 1L, 97L );
         HttpEntity<List<MetricsIngestItem>> entity = new HttpEntity<>(Arrays.asList(ingestItem), headers);
-        ResponseEntity<String> response = this.restTemplate.exchange(createURLWithPort("v2/analytics/topics/usage-tracking-objectstore"),
+        ResponseEntity<String> response = this.restTemplate.exchange(createUrl("v2/analytics/topics/usage-tracking-objectstore"),
                 HttpMethod.POST, entity, String.class);
 
         assertThat(response.getStatusCode(), is((HttpStatus.ACCEPTED)));
@@ -48,13 +54,24 @@ public class QueryTest {
         MetricsIngestItem ingestItem = new MetricsIngestItem("c3a49b88-6143-4f71-a586-d54ad345da3e", "1cc83dc2-3471-4492-bd1f-0d739b7b24f3", "us-east-1",
                 "test_store", "STORE", "API_EVENT", "2017-10-25T17:52:00.000Z", "0b7c83f9-0ffb-42f9-bbb8-813598e01e0a", 1L, 1L, 97L );
         HttpEntity<List<MetricsIngestItem>> entity = new HttpEntity<>(Arrays.asList(ingestItem), headers);
-        ResponseEntity<String> response = this.restTemplate.exchange(createURLWithPort("v2/analytics/topics/usage-tracking-objectstore"),
+        ResponseEntity<String> response = this.restTemplate.exchange(createUrl("v2/analytics/topics/usage-tracking-objectstore"),
                 HttpMethod.GET, entity, String.class);
 
         assertThat(response.getStatusCode(), is((HttpStatus.METHOD_NOT_ALLOWED)));
     }
 
-    private String createURLWithPort(String uri) {
-        return "https://as-common-ingest-int.qax.msap.io/" + uri;
+    @Test
+    public void testInvalidRequest() {
+        MetricsIngestItem ingestItem = new MetricsIngestItem("c3a49b88-6143-4f71-a586-d54ad345da3e", "1cc83dc2-3471-4492-bd1f-0d739b7b24f3", "us-east-1",
+                "test_store", "STORE", "API_EVENT", "2017-10-25T17:52:00.000Z", "0b7c83f9-0ffb-42f9-bbb8-813598e01e0a", 1L, 1L, 97L );
+        HttpEntity<MetricsIngestItem> entity = new HttpEntity<>(ingestItem, headers);
+        ResponseEntity<String> response = this.restTemplate.exchange(createUrl("v2/analytics/topics/usage-tracking-objectstore"),
+                HttpMethod.POST, entity, String.class);
+
+        assertThat(response.getStatusCode(), is((HttpStatus.INTERNAL_SERVER_ERROR)));
+    }
+
+    private String createUrl(String uri) {
+        return analyticsQueueUrl + uri;
     }
 }
---
+++ src/test/java/com/mulesoft/integration/springboot/client/action/AnalyticsQueryTest.java
@@ -0,0 +1,63 @@
+package com.mulesoft.integration.springboot.client.action;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@TestPropertySource("classpath:application-test.properties")
+public class AnalyticsQueryTest {
+
+
+    @Value("${analytics.query.url}")
+    private String analyticsQueueUrl;
+
+    private TestRestTemplate restTemplate = new TestRestTemplate();
+
+    HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    public void testAnalyticsQuery() {
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURL("events/1.0/c3a49b88-6143-4f71-a586-d54ad345da3e/1day"))
+                .queryParam("start_date", "2017-10-01T00:00:00")
+                .queryParam("end_date", "2017-10-26T00:00:00");
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(builder.build().toUriString(),
+                HttpMethod.GET, entity, String.class);
+
+        assertThat(response.getStatusCode(), is((HttpStatus.OK)));
+    }
+
+    @Test
+    public void testWrongQueryMethod() {
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURL("events/1.0/c3a49b88-6143-4f71-a586-d54ad345da3e/1day"))
+                .queryParam("start_date", "2017-10-01T00:00:00")
+                .queryParam("end_date", "2017-10-26T00:00:00");
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(builder.build().toUriString(),
+                HttpMethod.PUT, entity, String.class);
+
+        assertThat(response.getStatusCode(), is((HttpStatus.METHOD_NOT_ALLOWED)));
+    }
+
+    private String createURL(String uri) {
+        return analyticsQueueUrl + uri;
+    }
+}
