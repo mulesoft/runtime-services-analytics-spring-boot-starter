5894f581a0bf5965ea8ae4bfda761a8a13602d93
kapilduraphe <kapil.vduraphe@gmail.com>
2017-11-01 18:01:02 -0700

    adding tests with wiremock

--- src/test/java/com/mulesoft/integration/springboot/client/action/AnalyticsIngestTest.java
+++ src/test/java/com/mulesoft/integration/springboot/client/action/AnalyticsIngestTest.java
@@ -6,8 +6,10 @@
 package com.mulesoft.integration.springboot.client.action;
 
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.mulesoft.runtime.services.analytics.integration.model.MetricsIngestItem;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,8 +21,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -33,45 +35,33 @@ public class AnalyticsIngestTest {
 
     private TestRestTemplate restTemplate = new TestRestTemplate();
 
-    @Value("${analytics.ingest.url}")
-    private String analyticsQueueUrl;
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8090);
+
+    @Value("${analytics.test.ingest.url}")
+    private String analyticsIngestUrl;
 
     HttpHeaders headers = new HttpHeaders();
 
     @Test
     public void testIngestQuery() {
+
+        wireMockRule.stubFor(any(urlPathMatching("/v2/analytics/topics/usage-tracking-objectstore")).willReturn(aResponse().
+                withHeader("Content-Type", "text/json").withStatus(202).withBody("{}")));
+
+
         MetricsIngestItem ingestItem = new MetricsIngestItem("c3a49b88-6143-4f71-a586-d54ad345da3e", "1cc83dc2-3471-4492-bd1f-0d739b7b24f3", "us-east-1",
                 "test_store", "STORE", "API_EVENT", "2017-10-25T17:52:00.000Z", "0b7c83f9-0ffb-42f9-bbb8-813598e01e0a", 1L, 1L, 97L );
+
         HttpEntity<List<MetricsIngestItem>> entity = new HttpEntity<>(Arrays.asList(ingestItem), headers);
+
         ResponseEntity<String> response = this.restTemplate.exchange(createUrl("v2/analytics/topics/usage-tracking-objectstore"),
                 HttpMethod.POST, entity, String.class);
 
-        assertThat(response.getStatusCode(), is((HttpStatus.ACCEPTED)));
-    }
-
-    @Test
-    public void testIngestIncorrectMethodType() {
-        MetricsIngestItem ingestItem = new MetricsIngestItem("c3a49b88-6143-4f71-a586-d54ad345da3e", "1cc83dc2-3471-4492-bd1f-0d739b7b24f3", "us-east-1",
-                "test_store", "STORE", "API_EVENT", "2017-10-25T17:52:00.000Z", "0b7c83f9-0ffb-42f9-bbb8-813598e01e0a", 1L, 1L, 97L );
-        HttpEntity<List<MetricsIngestItem>> entity = new HttpEntity<>(Arrays.asList(ingestItem), headers);
-        ResponseEntity<String> response = this.restTemplate.exchange(createUrl("v2/analytics/topics/usage-tracking-objectstore"),
-                HttpMethod.GET, entity, String.class);
-
-        assertThat(response.getStatusCode(), is((HttpStatus.METHOD_NOT_ALLOWED)));
-    }
-
-    @Test
-    public void testInvalidRequest() {
-        MetricsIngestItem ingestItem = new MetricsIngestItem("c3a49b88-6143-4f71-a586-d54ad345da3e", "1cc83dc2-3471-4492-bd1f-0d739b7b24f3", "us-east-1",
-                "test_store", "STORE", "API_EVENT", "2017-10-25T17:52:00.000Z", "0b7c83f9-0ffb-42f9-bbb8-813598e01e0a", 1L, 1L, 97L );
-        HttpEntity<MetricsIngestItem> entity = new HttpEntity<>(ingestItem, headers);
-        ResponseEntity<String> response = this.restTemplate.exchange(createUrl("v2/analytics/topics/usage-tracking-objectstore"),
-                HttpMethod.POST, entity, String.class);
-
-        assertThat(response.getStatusCode(), is((HttpStatus.INTERNAL_SERVER_ERROR)));
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
     }
 
     private String createUrl(String uri) {
-        return analyticsQueueUrl + uri;
+        return analyticsIngestUrl + ":" + wireMockRule.port() + uri;
     }
 }
--- src/test/java/com/mulesoft/integration/springboot/client/action/AnalyticsQueryTest.java
+++ src/test/java/com/mulesoft/integration/springboot/client/action/AnalyticsQueryTest.java
@@ -5,10 +5,12 @@
  */
 package com.mulesoft.integration.springboot.client.action;
 
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,26 +18,32 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration
 @TestPropertySource("classpath:application-test.properties")
 public class AnalyticsQueryTest {
 
-
-    @Value("${analytics.query.url}")
-    private String analyticsQueueUrl;
+    @Value("${analytics.test.query.url}")
+    private String analyticsQueryUrl;
 
     private TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8090);
 
     HttpHeaders headers = new HttpHeaders();
 
     @Test
     public void testAnalyticsQuery() {
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURL("events/1.0/c3a49b88-6143-4f71-a586-d54ad345da3e/1day"))
+        wireMockRule.stubFor(any(urlPathMatching("/events/1.0/.*")).withQueryParam("start_date", containing("2017-10-01T00:00:00"))
+                .withQueryParam("end_date", containing("2017-10-26T00:00:00")).willReturn(aResponse().
+                withHeader("Content-Type", "text/json").withStatus(200).withBody("{}")));
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURL("/events/1.0/c3a49b88-6143-4f71-a586-d54ad345da3e/1day"))
                 .queryParam("start_date", "2017-10-01T00:00:00")
                 .queryParam("end_date", "2017-10-26T00:00:00");
 
@@ -44,25 +52,48 @@ public class AnalyticsQueryTest {
         ResponseEntity<String> response = restTemplate.exchange(builder.build().toUriString(),
                 HttpMethod.GET, entity, String.class);
 
-        assertThat(response.getStatusCode(), is((HttpStatus.OK)));
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
     @Test
-    public void testWrongQueryMethod() {
+    public void notMatchOnUrlPathWhenExtraPathStringPresent() {
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURL("events/1.0/c3a49b88-6143-4f71-a586-d54ad345da3e/1day"))
+        wireMockRule.stubFor(any(urlPathMatching("/events/1.0/.*")).withQueryParam("start_date", containing("2017-10-01T00:00:00"))
+                .withQueryParam("end_date", containing("2017-10-26T00:00:00")).willReturn(aResponse().
+                        withHeader("Content-Type", "text/json").withStatus(200).withBody("{}")));
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURL("/events/analytics/1.0/c3a49b88-6143-4f71-a586-d54ad345da3e/1day"))
                 .queryParam("start_date", "2017-10-01T00:00:00")
                 .queryParam("end_date", "2017-10-26T00:00:00");
 
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         ResponseEntity<String> response = restTemplate.exchange(builder.build().toUriString(),
-                HttpMethod.PUT, entity, String.class);
+                HttpMethod.GET, entity, String.class);
 
-        assertThat(response.getStatusCode(), is((HttpStatus.METHOD_NOT_ALLOWED)));
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
+    @Test
+    public void testInvalidQueryParam() {
+
+            wireMockRule.stubFor(any(urlPathMatching("/events/1.0/.*")).withQueryParam("start_date", containing("2017-10-01T00:00:00"))
+                    .withQueryParam("end", containing("2017-10-26T00:00:00")).willReturn(aResponse().
+                            withStatus(400)));
+
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(createURL("/events/1.0/c3a49b88-6143-4f71-a586-d54ad345da3e/1day"))
+                    .queryParam("start_date", "2017-10-01T00:00:00")
+                    .queryParam("end", "2017-10-26T00:00:00");
+
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(builder.build().toUriString(),
+                    HttpMethod.GET, entity, String.class);
+
+            assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        }
+
     private String createURL(String uri) {
-        return analyticsQueueUrl + uri;
+        return analyticsQueryUrl + ":" + wireMockRule.port() + uri;
     }
 }
