6d789a19271429aea3f62c36b92b08c7e3eb1a4d
kapilduraphe <kapil.vduraphe@gmail.com>
2017-11-03 17:39:10 -0700

    adding sonar profile in pom

--- src/main/java/com/mulesoft/runtime/services/analytics/integration/model/MetricsIngestItem.java
+++ src/main/java/com/mulesoft/runtime/services/analytics/integration/model/MetricsIngestItem.java
@@ -1,5 +1,5 @@
 /*
- * (c) 2003-2017 MuleSoft, Inc. This software is protected under international copyright law. All use of this software is subject to
+ * (c) 2003-2016 MuleSoft, Inc. This software is protected under international copyright law. All use of this software is subject to
  * MuleSoft's Master Subscription Agreement (or other Terms of Service) separately entered into between you and MuleSoft. If such an
  * agreement is not in place, you may not use the software.
  */
